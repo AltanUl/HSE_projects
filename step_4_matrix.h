d2b4de70044f4c3b2f030ed1047e62964d35cd8d
AltanUl <128617823+AltanUl@users.noreply.github.com>
2024-02-17 22:07:31 +0100

    Update step_4_matrix.h

--- step_4_matrix.h
+++ step_4_matrix.h
@@ -12,13 +12,17 @@ public:
     typedef std::vector<double> MatrixVector;
     typedef std::vector<MatrixVector> VectorOfMatrixVector;
     typedef std::pair<size_t, size_t> SizePair;
-
+    
     // default constructor
     Matrix();
+
+    // Constructor that creates a matrix of m rows and n columns
     Matrix(size_t m, size_t n, double def);
+
     static MatrixVector createVector(size_t sz, double val);
-    void resize(size_t m, size_t n, double def);
     
+    // method to resize the matrix
+    void resize(size_t m, size_t n, double def);
     SizePair getSize() const;
     
 private:
